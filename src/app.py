db753ccff7e2f3eba812b6c1c74d99fe8ba17325
avanegas <arco527@gmail.com>
2024-11-01 11:56:13 -0500

    Add configuration project

--- src/app.py
+++ src/app.py
@@ -11,15 +11,13 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-# from models import Person
-
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+# Database configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
@@ -31,55 +29,49 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
-# add the admin
+# Add the admin and commands
 setup_admin(app)
-
-# add the admin
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
+# Add all endpoints from the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-
-
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+# Serve admin root
+# @app.route('/admin', methods=['GET'])
+# def serve_admin_root():
+#     response = send_from_directory(static_file_dir, 'admin/index.html')
+#     response.cache_control.max_age = 0  # Avoid cache memory
+#     return response
 
-@app.route('/admin', methods=['GET'])
-def serve_any_other_file(path):
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
-    return response
-
+# Serve admin files
 @app.route('/admin/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
+def serve_admin_file(path):
+    response = send_from_directory(static_file_dir, f'admin/{path}')
+    response.cache_control.max_age = 0  # Avoid cache memory
     return response
 
+# Serve public files
 @app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
+def serve_public_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
+    response.cache_control.max_age = 0  # Avoid cache memory
     return response
 
-
-# this only runs if `$ python src/main.py` is executed
+# This only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
